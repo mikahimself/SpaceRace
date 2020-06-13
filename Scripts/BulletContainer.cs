646858affdf10809495bffd4642872ee0edcd851
mika <mika.laihanen@gmail.com>
2020-06-13 22:26:22 +0300

    Add bullet and spaceship animations.

--- Scripts/BulletContainer.cs
+++ Scripts/BulletContainer.cs
@@ -5,7 +5,7 @@ public class BulletContainer : Node2D
 {
     private PackedScene bulletScene;
     private int[] _startPositions = {0, 1024};
-    private int _speed = 175;
+    private int _speed = 200;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -23,11 +23,12 @@ public class BulletContainer : Node2D
             bullet.speed = _speed;
             int shuffle = (int)Godot.GD.RandRange(50, 1024);
             if (i % 2 == 0) {
-                bullet.Position = new Vector2(_startPositions[0] + shuffle, startPosY * i + 10);
+                bullet.Position = new Vector2(_startPositions[0] + shuffle, startPosY * i + 14);
                 bullet.direction = 1;
             } else {
-                bullet.Position = new Vector2(_startPositions[1] - shuffle, startPosY * i + 10);
+                bullet.Position = new Vector2(_startPositions[1] - shuffle, startPosY * i + 14);
                 bullet.direction = -1;
+                bullet.RotationDegrees = 180;
             }
             AddChild(bullet);
         }
